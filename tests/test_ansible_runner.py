1fabbb76088df1ff25bba5c7f0fbe79437074839
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-31 22:19:36 +1000

    Update test_ansible_runner.py

--- tests/test_ansible_runner.py
+++ tests/test_ansible_runner.py
@@ -1,1 +1,6 @@
 
+from src.ansible_runner import run_playbook
+
+def test_playbook():
+    result = run_playbook("examples/ansible_playbooks/setup_server.yml")
+    assert result["status"] == "success"
